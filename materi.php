08859b067b25b5e328586722283d35e16979d20f
IsmailFikri12008 <203040008@mail.unpas.ac.id>
2022-10-17 11:48:58 +0700

    menyelesaikan video 1 (intro)

---
+++ materi.php
@@ -0,0 +1,54 @@
+<!-- Laravel
+
+    "Laravel is a web application framework with expressive, elegant syntax."
+    Sebuah kerangka aplikasi dengan sintak elegan dan ekspresif.
+
+    Wen application Framework
+    - Sebuah 'kerangka' yang didesain untuk mendukung pembangunan aplikasi web
+    - Menyediakan standar / cara untuk membangun aplikasi web
+    - Bertujuan untuk mengotomasi hal-hal umum yang biasanya dilakukan saat membangun aplikasi web
+    - Contohnya : Database Library, Templating Engine, Session Management, Authentication, Security, dll
+    
+    Laravel 
+    Dibuat oleh Taylor Otwell pada Juni 2011 Sebagai alternatif framework untuk Condelgniter dan versi terbarunya versi 8
+
+    Filosofi Laravel
+    We Believe development must be an enjoyable, creative experience to be truly fulfilling
+
+    Tujuam Laravel
+    Laravel aims to make the development process a pleasing one for the developer without sacrificing application functionally. Happy developers make the best code
+    Untuk membuat proses pengembangan itu bisa dinikmati oleh developer nya tanpa harus mengorbankan fungsionalitas aplikasinya. Karena ketika developer bahagia akan menghasilkan code yang terbaik.
+
+    Fitur utama Laravel
+    - MVC
+    - Templating Engine
+    - Artisan Console
+    - Eloquent ERM
+    - Authentication & Authorization
+    - Testing
+    - Packing System
+    - Multiple File System
+    - Task Scheduling
+    - Websocket Programming
+
+    Pre-requisite
+    - PHP Dasar
+    - Object Oriented Programming
+    - Konsep MVP
+
+    Requirement
+    - PHP 7.4 / 8
+    - MySQL
+    - Composer 2
+    - SequelPro / MySQL WorkBench
+    - Terminal / CMD / PowerShell / GitBash
+    - Code Editor : Visual Studio Code
+    - Extentions
+        - PHP Intelephense
+        - Laravel Artisan
+        - Laravel Snippets
+        - Laravel Blade Snippets
+        - Laravel Blade Spacer
+        - Laravel GoTo View
+
+-->
