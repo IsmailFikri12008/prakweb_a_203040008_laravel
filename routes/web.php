60d7a0119d82152947ba73f156f1bb7daa240878
IsmailFikri12008 <203040008@mail.unpas.ac.id>
2022-10-17 18:25:30 +0700

    menyelesaikan video 4 (blade templating engine)

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view ('home');
+    return view ('home', [
+        "title" => "Home"
+    ]);
 });
 
 Route::get('/about', function () {
     return view ('about', [
+        "title" => "About",
         "name" => "Ismail Fikri",
         "email" => "203040008@mail.unpas.ac.id",
         "image" => "ppgoogle.jpg"
@@ -26,5 +29,53 @@ Route::get('/about', function () {
 });
 
 Route::get('/blog', function () {
-    return view ('posts');
+    $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Ismail Fikri",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. A vero, non harum dignissimos doloribus qui illum odit vitae ea numquam tenetur illo culpa nostrum, enim assumenda quos iusto veritatis. Consequuntur? Lorem ipsum dolor sit amet consectetur adipisicing elit. A vero, non harum dignissimos doloribus qui illum odit vitae ea numquam tenetur illo culpa nostrum, enim assumenda quos iusto veritatis. Consequuntur? Lorem ipsum dolor sit amet consectetur adipisicing elit. A vero, non harum dignissimos doloribus qui illum odit vitae ea numquam tenetur illo culpa nostrum, enim assumenda quos iusto veritatis. Consequuntur?"
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Fikri Ismail",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. A vero, non harum dignissimos doloribus qui illum odit vitae ea numquam tenetur illo culpa nostrum, enim assumenda quos iusto veritatis. Consequuntur? Lorem ipsum dolor sit amet consectetur adipisicing elit. A vero, non harum dignissimos doloribus qui illum odit vitae ea numquam tenetur illo culpa nostrum, enim assumenda quos iusto veritatis. Consequuntur? Lorem ipsum dolor sit amet consectetur adipisicing elit. A vero, non harum dignissimos doloribus qui illum odit vitae ea numquam tenetur illo culpa nostrum, enim assumenda quos iusto veritatis. Consequuntur? Lorem ipsum dolor sit amet consectetur adipisicing elit. A vero, non harum dignissimos doloribus qui illum odit vitae ea numquam tenetur illo culpa nostrum, enim assumenda quos iusto veritatis. Consequuntur?"
+        ]
+    ];
+    return view ('posts', [
+        "title" => "Posts",
+        "posts" => $blog_posts
+    ]);
+});
+
+// Halaman single post
+Route::get('posts/{slug}', function($slug) {
+    $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Ismail Fikri",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. A vero, non harum dignissimos doloribus qui illum odit vitae ea numquam tenetur illo culpa nostrum, enim assumenda quos iusto veritatis. Consequuntur? Lorem ipsum dolor sit amet consectetur adipisicing elit. A vero, non harum dignissimos doloribus qui illum odit vitae ea numquam tenetur illo culpa nostrum, enim assumenda quos iusto veritatis. Consequuntur? Lorem ipsum dolor sit amet consectetur adipisicing elit. A vero, non harum dignissimos doloribus qui illum odit vitae ea numquam tenetur illo culpa nostrum, enim assumenda quos iusto veritatis. Consequuntur?"
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Fikri Ismail",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. A vero, non harum dignissimos doloribus qui illum odit vitae ea numquam tenetur illo culpa nostrum, enim assumenda quos iusto veritatis. Consequuntur? Lorem ipsum dolor sit amet consectetur adipisicing elit. A vero, non harum dignissimos doloribus qui illum odit vitae ea numquam tenetur illo culpa nostrum, enim assumenda quos iusto veritatis. Consequuntur? Lorem ipsum dolor sit amet consectetur adipisicing elit. A vero, non harum dignissimos doloribus qui illum odit vitae ea numquam tenetur illo culpa nostrum, enim assumenda quos iusto veritatis. Consequuntur? Lorem ipsum dolor sit amet consectetur adipisicing elit. A vero, non harum dignissimos doloribus qui illum odit vitae ea numquam tenetur illo culpa nostrum, enim assumenda quos iusto veritatis. Consequuntur?"
+        ]
+    ];
+
+    $new_post = [];
+
+    foreach ($blog_posts as $post) {
+        if($post['slug'] === $slug) {
+            $new_post = $post;
+        }
+    }
+
+    return view('post', [
+        "title" => "Single Post",
+        "post" => $new_post
+    ]);
 });
